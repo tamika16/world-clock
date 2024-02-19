0e2bb8d4b0aadbd2460733fad379439f02bb175d
tamika16 <147372830+tamika16@users.noreply.github.com>
2024-02-19 03:22:37 -0500

    made the select element functional

--- src/index.js
+++ src/index.js
@@ -1,24 +1,49 @@
 function updateTime() {
   let pittsburghElement = document.querySelector("#pittsburgh");
-  let pittsburghDateElement = pittsburghElement.querySelector(".date");
-  let pittsburghTimeElement = pittsburghElement.querySelector(".time");
-  let pittsburghTime = moment().tz("America/New_York");
+  if (pittsburghElement) {
+    let pittsburghDateElement = pittsburghElement.querySelector(".date");
+    let pittsburghTimeElement = pittsburghElement.querySelector(".time");
+    let pittsburghTime = moment().tz("America/New_York");
 
-  pittsburghDateElement.innerHTML = pittsburghTime.format("MMMM Do, YYYY");
-  pittsburghTimeElement.innerHTML = pittsburghTime.format(
-    "hh:mm:ss [<small>]A[</small>]"
-  );
+    pittsburghDateElement.innerHTML = pittsburghTime.format("MMMM Do, YYYY");
+    pittsburghTimeElement.innerHTML = pittsburghTime.format(
+      "hh:mm:ss [<small>]A[</small>]"
+    );
+  }
 
   let seoulElement = document.querySelector("#seoul");
-  let seoulDateElement = seoulElement.querySelector(".date");
-  let seoulTimeElement = seoulElement.querySelector(".time");
-  let seoulTime = moment().tz("Asia/Seoul");
+  if (seoulElement) {
+    let seoulDateElement = seoulElement.querySelector(".date");
+    let seoulTimeElement = seoulElement.querySelector(".time");
+    let seoulTime = moment().tz("Asia/Seoul");
 
-  seoulDateElement.innerHTML = seoulTime.format("MMMM Do, YYYY");
-  seoulTimeElement.innerHTML = seoulTime.format(
-    "hh:mm:ss [<small>]A[</small>]"
-  );
+    seoulDateElement.innerHTML = seoulTime.format("MMMM Do, YYYY");
+    seoulTimeElement.innerHTML = seoulTime.format(
+      "hh:mm:ss [<small>]A[</small>]"
+    );
+  }
 }
 
 updateTime();
 setInterval(updateTime, 1000);
+
+function updateCity(event) {
+  let cityTimezone = event.target.value;
+  let cityTime = moment().tz(cityTimezone);
+  let cityName = event.target[event.target.selectedIndex].text;
+  let cityDisplayElement = document.querySelector("#cities-display");
+  cityDisplayElement.innerHTML = `
+  <div class="city">
+        <div>
+        <h2>${cityName}</h2>
+        <div class="date">${cityTime.format("MMMM Do, YYYY")}</div>
+        </div>
+        <div class="time">${cityTime.format(
+          "hh:mm:ss"
+        )} <small>${cityTime.format("A")}</div>
+    </div>
+  `;
+}
+
+let citySelectElement = document.querySelector("#city-select");
+citySelectElement.addEventListener("change", updateCity);
